909107291454c1bf3679b34122ed89be19d6702c
ManavJoshi <manavjoshi154@gmail.com>
2023-12-30 20:32:02 +0530

    added scss file for customizing bootstrap :ok_hand:

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <p className="h1 text-center p-3 text-decoration-underline">Home</p>
+    </>
+  );
+};
+
+export default Home;
