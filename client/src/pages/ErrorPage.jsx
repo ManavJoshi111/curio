a6490d03a95b2ab446fbb53ebce21b3d24995357
ManavJoshi111 <manavjoshi154@gmail.com>
2024-04-12 21:42:16 +0530

    removed the random line

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../styles/ErrorPage.css";
 
-// this is a random commit in the main branch to know if I an push or not
 const ErrorPage = () => {
   return (
     <div className="error-page">
